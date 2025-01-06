ae780481408e25a4124f244c8c9332cbaacc7872
OmidJalali84 <omid.jalali.773@gmail.com>
2025-01-06 21:29:21 +0330

    debug dashboard

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -121,205 +121,96 @@ function Dashboard() {
   }, [address]);
 
   return (
-    <div className="py-4 md:py-20 mx-auto max-w-6xl px-4 sm:px-6">
+    <div className="p-4 md:p-20 px-4 sm:px-6">
       {isConnecting && (
-        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-4xl md:text-5xl font-bold">
+        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-2xl sm:text-3xl md:text-5xl font-bold">
           Connecting...
         </p>
       )}
       {isDisconnected && (
-        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-4xl md:text-5xl font-bold">
+        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-2xl sm:text-3xl md:text-5xl font-bold">
           Wallet is disconnected
         </p>
       )}
       {address && !userInfo && !error && (
-        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-4xl md:text-5xl font-bold">
+        <p className="animate-gradient bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 bg-clip-text text-transparent text-center text-2xl sm:text-3xl md:text-5xl font-bold">
           Fetching user information...
         </p>
       )}
       {error && (
-        <p className="text-red-400 text-center text-lg font-semibold">
+        <p className="text-red-400 text-center text-sm sm:text-lg font-semibold">
           {error}
         </p>
       )}
       {userInfo && (
-        <div className=" p-8 rounded-lg shadow-lg">
-          <h3 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-8">
+        <div className="p-6 sm:p-8 rounded-lg shadow-lg">
+          <h3 className="text-3xl sm:text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-6 sm:mb-8">
             User Information
           </h3>
           <ul className="space-y-4">
-            <div className="p-8 rounded-lg shadow-lg">
-              <h3 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-8">
-                User Information
-              </h3>
-              <ul className="space-y-4">
-                {/* Basic Information */}
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">Upline:</span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.upline}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Register Time:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTime(userInfo.registerTime)}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Last Withdrawal:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTime(userInfo.lastWithdrawal)}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Unlocked Levels:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.unlockedLevels}
-                  </span>
-                </li>
-
-                {/* Financials */}
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Entry Amount:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.entryAmount)} $
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Entry Token:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.entryToken)} GWT
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Total Frozen Tokens:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.totalFrozenTokens)} GWT
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Total Reward:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.totalReward)} $
-                  </span>
-                </li>
-
-                {/* Directs and Rewards */}
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Directs:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.directs}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Right Count:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.rightCount}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Right Reward:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.rightReward)} $
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Middle Count:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.middleCount}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Middle Reward:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.middleReward)} $
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Left Count:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {userInfo.leftCount}
-                  </span>
-                </li>
-                <li className="text-lg text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-4 py-3 shadow-md">
-                  <span className="font-semibold text-indigo-300">
-                    Left Reward:
-                  </span>
-                  <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100">
-                    {formatTokens(userInfo.leftReward)} $
-                  </span>
-                </li>
-              </ul>
-            </div>
+            {Object.entries(userInfo).map(([key, value]) => (
+              <li
+                className="sm:text-base text-indigo-200 flex flex-col sm:flex-row justify-between items-start sm:items-center bg-indigo-900/50 rounded-lg px-3 py-3 shadow-md"
+                key={key}
+              >
+                <span className="font-semibold text-indigo-300">{key}:</span>
+                <span className="mt-1 sm:mt-0 sm:ml-4 text-indigo-100 text-[10px] sm:text-xs">
+                  {key.toLowerCase().includes("time")
+                    ? formatTime(String(value))
+                    : key.toLowerCase().includes("token")
+                    ? formatTokens(String(value))
+                    : key.toLowerCase().includes("reward")
+                    ? formatTokens(String(value))
+                    : key.toLowerCase().includes("amount")
+                    ? formatTokens(String(value))
+                    : key.toLowerCase().includes("last")
+                    ? formatTime(String(value))
+                    : String(value)}
+                </span>
+              </li>
+            ))}
           </ul>
 
-          <h3 className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-9 mt-9">
+          <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-6 sm:mb-9 mt-6 sm:mt-9">
             upgrade Your Wealth Plan
           </h3>
-          <ul>
-            <li className="mb-5 text-xl text-indigo-200/65">
-              &#8226; Every upgrade brings you closer to your financial goals.
+          <ul className="list-disc space-y-3 sm:space-y-4 px-5 sm:px-8 text-indigo-200/65 text-sm sm:text-xl">
+            <li> Every upgrade brings you closer to your financial goals.</li>
+            <li>
+              {" "}
+              Enhance your journey to financial growth by upgrading your plan!
             </li>
-            <li className="mb-5 text-xl text-indigo-200/65">
-              &#8226; Enhance your journey to financial growth by upgrading your
-              plan!
-            </li>
-            <li className="mb-5 text-xl text-indigo-200/65">
-              &#8226; Increase your entry amount to unlock higher levels and
-              maximize your rewards.
+            <li>
+              Increase your entry amount to unlock higher levels and maximize
+              your rewards.
             </li>
           </ul>
           {isModalOpen && <UpgradePlanModal />}
         </div>
       )}
-      <div className="p-8 rounded-lg shadow-lg">
-        <h3 className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-9">
+      <div className="p-6 sm:p-8 rounded-lg shadow-lg mt-6 sm:mt-10">
+        <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-200 text-center mb-6 sm:mb-9">
           Withdraw your rewards
         </h3>
-        <ul>
-          <li className="mb-5 text-xl text-indigo-200/65">
-            &#8226; Claim the interest you’ve earned on your GWT tokens!{" "}
-          </li>
-          <li className="mb-5 text-xl text-indigo-200/65">
-            &#8226; Withdrawals are available once a month to keep your earnings
+        <ul className="list-disc space-y-3 sm:space-y-4 px-5 sm:px-8 text-indigo-200/65 text-sm sm:text-xl">
+          <li> Claim the interest you’ve earned on your GWT tokens! </li>
+          <li>
+            {" "}
+            Withdrawals are available once a month to keep your earnings
             growing.
           </li>
         </ul>
         <div className="text-center">
-          <ul className="space-y-4">
+          <ul className="space-y-4 sm:space-y-4">
             <span className="mb-5 text-xl text-indigo-200/65">
               Next withdraw:{" "}
             </span>
             {userInfo &&
               Object.entries(userInfo).map(([key, value]) => (
-                <span key={key} className="mb-5 text-xl text-indigo-200/65">
+                <span
+                  key={key}
+                  className="text-sm sm:text-xl text-indigo-200/65"
+                >
                   {key.toLowerCase().includes("lastwithdrawal")
                     ? formatTime(String(value + BigInt(2592000)))
                     : null}
@@ -335,7 +226,7 @@ function Dashboard() {
           </button>
           {withdrawMessage && (
             <p
-              className={`mt-4 text-center ${
+              className={`mt-4 text-center text-sm sm:text-base ${
                 withdrawMessage.includes("success")
                   ? "text-green-400"
                   : "text-red-400"
