02b3320ce2b50d7e8a9ec21e6d740feca52873cb
Omid Jalali <151427905+OmidJalali84@users.noreply.github.com>
2025-07-11 19:47:54 +0330

    Update Web3Provider.jsx

--- components/web3/Web3Provider.jsx
+++ components/web3/Web3Provider.jsx
@@ -1,14 +1,16 @@
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { arbitrumSepolia } from "wagmi/chains";
+import { arbitrumSepolia, tron } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
 export const config = createConfig(
   getDefaultConfig({
-    chains: [arbitrumSepolia],
+    chains: [arbitrumSepolia,tron],
     transports: {
       [arbitrumSepolia.id]: http(
         `https://arb-sepolia.g.alchemy.com/v2/7PW6w16NTzgdT0NiWUFLJxLUL5XHGTMz`
+      ),[tron.id]: http(
+        `https://api.trongrid.io/jsonrpc`
       ),
     },
     walletConnectProjectId: " 7PW6w16NTzgdT0NiWUFLJxLUL5XHGTMz",
